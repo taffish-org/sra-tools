c5af53ed2ad712b82905d8bfafd4feb3f86bde03
Kaiyuan Han <hermit_han@163.com>
2025-03-08 16:24:00 +0800

    Initial commit

---
+++ sra-tools.info.lisp
@@ -0,0 +1,33 @@
+(:name           "sra-tools")
+(:repo           "taffish-org/sra-tools")
+(:author         "KaiyuanHan")
+(:version        "3.2.0")
+(:intro          "The SRA Toolkit and SDK from NCBI is a collection of tools and libraries for using data in the INSDC Sequence Read Archives.")
+(:dependencies)
+(:functions      "abi-dump"
+                 "align-info"
+                 "cache-mgr"
+                 "check-corrupt"
+                 "fasterq-dump"
+                 "fastq-dump"
+                 "illumina-dump"
+                 "kdbmeta"
+                 "ngs-pileup"
+                 "prefetch"
+                 "rcexplain"
+                 "ref-variation"
+                 "sam-dump"
+                 "sff-dump"
+                 "sra-info"
+                 "sra-pileup"
+                 "sra-search"
+                 "sra-stat"
+                 "srapath"
+                 "sratools"
+                 "test-sra"
+                 "var-expand"
+                 "vdb-config"
+                 "vdb-decrypt"
+                 "vdb-dump"
+                 "vdb-encrypt"
+                 "vdb-validate")
